c12ecc5c9f631aa13493518cf1b4f8e7086610d9
tishagera11 <94456283+tishagera11@users.noreply.github.com>
2024-09-11 06:37:33 +0000

    changes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("3 + 3") // we are using node js, not browser
+
+console.log(3 + 3) // code readibility should be high
+
+let name = "Tisha"
+let age = "21"
+let isLoggedIn = false
+let state;
+
+// number - 2 to power 53
+//bigint - tading, stock market for very big numbers
+// string - ""
+// boolean - true/ false
+// null - stand alone value
+// undefined - value not assigned
+// symbol - unique 
+
+
+// OBJECT
+
+console.log(typeof "Tisha");
+console.log(typeof null);
+console.log(typeof undefined);
