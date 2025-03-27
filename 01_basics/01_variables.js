1ea8051ef4061e967ebe603d4cf694cbc937850c
tishagera11 <94456283+tishagera11@users.noreply.github.com>
2025-03-27 07:21:16 +0000

    changes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,10 +1,12 @@
-const accountId = 144553
+const accountId = 144553        
 let accountEmail = "tishagera2002@gmail.com"
-var accountPassword = "12345"
-accountCity = "Jaipur"
-let accountState;
+var accountPassword = "12345"       
+accountCity = "Jaipur"      //it is possible to delare variable without let and const also but is not recommended
+let accountState;       //value not defined 
 
 // accountId = 2 // not allowed
+// const = cannot be changed 
+// let = can be reassigned 
 
 accountEmail = "hc@hc.com"
 accountPassword = "212121212"
@@ -16,6 +18,7 @@ console.log(accountId);
 Prefer not to use var because of issue in block scope and functional scope
 */
 
-console.table([accountId, accountPassword, accountCity, accountState])
+console.table([accountId, accountPassword, accountCity, accountState])      
+//console.table gives all the mentioned values in tabular form
 
 
--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -1,10 +1,10 @@
-//let score = "33"
+let score = "33"
 
-//console.log(typeof score);
-//console.log(typeof (score));
+console.log(typeof score);
+console.log(typeof (score));
 
-//let valueInNumber = Number(score)
-//console.log(typeof valueInNumber);
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
 
 
 //"33"= 33
@@ -12,21 +12,21 @@
 // true = 1; false = 0
 
 
-//let isLoggedIn = 1
+let isLoggedIn = 1
 
-//let booleanIsLoggedIn = Boolean(isLoggedIn)
-//console.log(booleanIsLoggedIn);
+let booleanIsLoggedIn = Boolean(isLoggedIn)     //converting 1 into boolean 
+console.log(booleanIsLoggedIn);
 
 // 1 = true ; 0= false
-// " "= false
+// " "= false       //empty string is false
 // "tisha" = true
 
-//let someNumber = 33
+let someNumber = 33
 
-//let stringNumber = String(someNumber)
-//console.log(stringNumber);
-//console.log(typeof stringNumber);
-//
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+ 
 
 // ******************OPERATIONS**************************
 
@@ -35,12 +35,12 @@ let value = 3
 let negValue = -value
 console.log(negValue);
 
-//console.log(2 + 2);
+/*console.log(2 + 2);
 console.log(2 - 2);
-console.log(2 * 2);
+console.log(2 * 2);             //basic operations
 console.log(2 ** 2);
 console.log(2 / 2);
-console.log(2 % 2);
+console.log(2 % 2);*/
 
 let str1 = "hello"
 let str2 = "TIsha"
@@ -50,20 +50,20 @@ console.log(str3);
 
 console.log("1" + 2);
 console.log(1 + "2");
-console.log("1" + 2 + 2);
-console.log(1 + 2 + "2");
+console.log("1" + 2 + 2);       //if string is first then all will be add in form of string
+console.log(1 + 2 + "2");       //if number is first then it will add number and then add string
 
 console.log(3 + 4 * 5 % 3);
 
-console.log(+true);
+console.log(+true);             
 console.log(+"");
 
 let num1, num2, num3
-num1 = num2 = num3 = 2 + 2
+num1 = num2 = num3 = 2 + 2      //not a good way to write code
 
 
 let gameCounter = 100
-++gameCounter;
+++gameCounter;              //prefix opeartor 
 console.log(gameCounter);
 
 
