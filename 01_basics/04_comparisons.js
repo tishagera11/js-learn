4a4113926da743edb64b67f578e3aadae7ef1ec3
tishagera11 <94456283+tishagera11@users.noreply.github.com>
2025-03-27 07:29:04 +0000

    changes

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -1,16 +1,16 @@
-console.log("2" > 1);
+console.log("2" > 1);               //datatype should alwyas be same in conversion
 console.log("02" > 1);
 
 console.log(null > 0);               // avoid such comaprisons
-console.log(null == 0);
-console.log(null >= 0);
+console.log(null == 0);             //==  & comparison work differently
+console.log(null >= 0);             //comparisons convert null into 0 thats why this is true  
 
-console.log(undefined == 0);
+console.log(undefined == 0);          
 console.log(undefined > 0);
 console.log(undefined < 0);
 
 
 
-// === strict check it does the datatypes also
+// === is strict check: it does check the datatypes also
 
 console.log("2" === 2);
