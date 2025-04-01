318a5b6e6ecca1179d51e7e38b810bff6fc524fc
tishagera11 <94456283+tishagera11@users.noreply.github.com>
2025-04-01 08:35:16 +0000

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// immediately invoked function expressions (iife)
+
+(function chai(){           //named iife
+    console.log(`DB CONNECTED`)
+})();         //wrapped the whole function in paranthesis with another paranthesis to execute the function
+//always end an iife function with semi colon to write another iife
+
+
+
+( (name) => {               //unmanem //arrow function iife
+    console.log(`DB CONNECTED TWO ${name}`)
+})('tisha')
